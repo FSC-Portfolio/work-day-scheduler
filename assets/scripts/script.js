dbe87e5bad478a926660fca89bff5163cfd3faca
jayarghargh <jayarghargh@gmail.com>
2020-12-13 07:48:28 +1030

    created determineTextareaClass to allow classes to be determined by the time

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,8 @@
 "use strict";
 
 // CONSTANTS
-const TIME_START = 9;  // Ahh to start at nine!
-const TIME_FINISH = 18;  // This poor sod works through to six pm.
+const TIME_START = 6;  // Ahh to start at nine!
+const TIME_FINISH = 14;  // This poor sod works through to six pm.
 
 // VARIABLES
 let dayStart;
@@ -15,7 +15,24 @@ function displayTime(displayElement) {
 }
 
 function setMomentToZero(hourToAdjust) {
+    // Sets the minutes and seconds to zeroes.
     return moment().set({'hour': hourToAdjust, 'minute': "00", 'seconds': "00"});
+}
+
+function determineTextareaClass(timeIn) {
+    // Returns the class for an objects css based by comparing current time to elements time.
+    let textareaClass;
+    // Test for present.
+    if (moment().get("hour") === moment(timeIn).get("hour")) {
+        textareaClass = "present";
+    // test for future time.
+    } else if (moment().isBefore(moment(timeIn))) {
+        textareaClass = "future";
+    // Test for past time.
+    } else if (moment().isAfter(moment(timeIn))) {
+        textareaClass = "past";
+    }
+    return textareaClass;
 }
 
 function createTimeBlock() {
@@ -23,15 +40,18 @@ function createTimeBlock() {
     // TODO attach a moment to each block... then it's almost done!
     let i = 0;
     while (i < TIME_FINISH - TIME_START) {
+        // correct the hour for this block.
+        let blockMoment = setMomentToZero(TIME_START + i);
+        // determine the correct class for the textarea
+
         // Assemble the parts for each row.
         let rowTimeBlock = $('<div class="row">');
         let divHour = $('<div class="col-1 hour">');
-        let textarea = $('<textarea class="col-10 future description">');
-        let saveButton = $('<div class="col-1 saveBtn">')
-        //  Assign a moment to each block.
-        let blockMoment = setMomentToZero(TIME_START + i);
+        let textarea = $('<textarea class="col-10 description">');
+        let saveButton = $('<div class="col-1 saveBtn">');
 
         // Carefully construct the row.
+        textarea.addClass(determineTextareaClass(blockMoment));
         saveButton.html('<a href="#"><i class="far fa-save"></i></a>');
         divHour.html('<span>' + blockMoment.format("hA") + '</span>');
         textarea.text("okies");

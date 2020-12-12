c855fba3b88b8d745b2517084d82d05122b38b6e
jayarghargh <jayarghargh@gmail.com>
2020-12-12 21:29:56 +1030

    created simple function to update the main page timeblock.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+// CONSTANTS
+
+// VARIABLES
+
+// FUNCTIONS
+function updateTimeBlock(timeblock) {
+    // Update any element with the date in format specified below.
+    $(timeblock).text(moment().format('dddd, MMMM Do'))
+}
+
+
+// STATEMENTS
+updateTimeBlock("#currentDay");
